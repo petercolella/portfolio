dc1a334aabeba57a3c031b955d09ee404c570e30
petercolella <peter.joseph.colella@gmail.com>
2018-09-27 00:54:03 -0400

    copy of about img element same size and behind original

--- js/test.js
+++ js/test.js
@@ -5,3 +5,10 @@ console.log('About Img: ', aboutImg);
 // 
 console.log('About Img Height: ', aboutImg.clientHeight);
 console.log('About Img Width: ', aboutImg.clientWidth);
+const aboutImgHeight = aboutImg.clientHeight;
+const aboutImgWidth = aboutImg.clientWidth;
+const aboutImgDivCopy = document.getElementById('about-img-div-copy');
+aboutImgDivCopy.style.height = `${aboutImgHeight}px`;
+aboutImgDivCopy.style.width = `${aboutImgWidth}px`;
+console.log('About Img Height Const: ', aboutImgHeight);
+console.log('About Img Width Const: ', aboutImgWidth);
