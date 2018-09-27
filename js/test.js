2e06d8932f980abda9e7d9d801460d47027e82ab
petercolella <peter.joseph.colella@gmail.com>
2018-09-26 23:59:30 -0400

    got height and width of about image element

--- js/test.js
+++ js/test.js
@@ -1,3 +1,7 @@
-$(document).ready(function() {
-    console.log('Page Loaded!')
-})
+// $(document).ready(function() {
+console.log('Page Loaded!');
+const aboutImg = document.getElementById('about-img');
+console.log('About Img: ', aboutImg);
+// 
+console.log('About Img Height: ', aboutImg.clientHeight);
+console.log('About Img Width: ', aboutImg.clientWidth);
